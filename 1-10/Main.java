20ba6de850998aec56674ee247d2a96bb370567c
ted <82011813+unnotedme@users.noreply.github.com>
2023-01-21 15:31:58 -0400

    - day four

---
+++ 1-10/Main.java
@@ -0,0 +1,8 @@
+public class Main { 
+    public static void main(String[] args) { 
+        System.out.println("Classes: Blueprints of objects, attributes and methods.");
+        System.out.println("Objects: Instances of classes.");
+        System.out.println("Attributes: Represent the state of an object.");
+        System.out.println("Methods: Describes the behavior of an object");
+    }
+}
