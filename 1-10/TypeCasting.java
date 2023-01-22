d560ff642788d631de6674dbc7ac5a39cb587808
ted <82011813+unnotedme@users.noreply.github.com>
2023-01-22 18:08:07 -0400

    - day 5

---
+++ 1-10/TypeCasting.java
@@ -0,0 +1,13 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        // Implicit casting
+        int x = 5;
+        double y = x;
+        System.out.println(y);
+
+        // Explicit casting
+        double a = 1.5;
+        int b = (int) a + 2;
+        System.out.println(b);
+    }
+}
